4f0215853d18d8b737671b4a7afe9894586e4a62
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2022-03-30 16:19:07 +0100

    Fixups and path updates to group lemon

--- 03_lemon_group.py
+++ 03_lemon_group.py
@@ -14,8 +14,7 @@ from anamnesis import obj_from_hdf5file
 sys.path.append('/Users/andrew/src/qlt')
 import qlt
 
-outdir = '/Users/andrew/Projects/glm/glm_psd/analysis'
-figbase = '/Users/andrew/Projects/glm/glm_psd/figures/'
+from glm_config import cfg
 
 #%% ---------------------------------------------------
 # Load single subject for reference
@@ -36,12 +35,13 @@ raw = dataset['raw'].pick_types(eeg=True)
 #%% --------------------------------------------------
 # Load first level results and fit group model
 
-inputs = '/Users/andrew/Projects/glm/glm_psd/analysis/group_sensorglm.hdf5'
+inputs = os.path.join(cfg['lemon_analysis_dir'], 'lemon_eeg_sensorglm_groupdata.hdf5')
 
 data = obj_from_hdf5file(inputs, 'data')
 with h5py.File(inputs, 'r') as F:
     #freq_vect = F['freq_vect'][()]  # needs fixing server-side
-    freq_vect = np.linspace(0.5, 48, 96)
+    freq_vect = np.linspace(0.5, 100, 200)
+    #freq_vect = np.linspace(0.5, 48, 96)
 
 # Drop obvious outliers
 bads = sails.utils.detect_artefacts(data.data[:, 0, :, :], axis=0)
@@ -51,7 +51,8 @@ clean_data = data.drop(np.where(bads)[0])
 df = pd.read_csv('/Users/andrew/Projects/ntad/RA_Interview/LEMON_RA_InterviewData2.csv')
 age = []
 sex = []
-for idx, subj in enumerate(data.info['subjs']):
+for idx, subj in enumerate(data.info['subj_id']):
+    subj = 'sub-' + subj
     ind = np.where(df['ID'] == subj)[0][0]
     row = df.iloc[ind]
     age.append(row['Age'] + 2.5)
@@ -59,7 +60,7 @@ for idx, subj in enumerate(data.info['subjs']):
 
 data.info['age'] = age
 data.info['sex'] = sex
-data.info['subjs'] = list(data.info['subjs'])
+data.info['subj_id'] = list(data.info['subj_id'])
 
 keeps = np.where(np.array(age) < 45)[0][:48]
 drops = np.setdiff1d(np.arange(len(age)), keeps)
@@ -77,17 +78,17 @@ design = DC.design_from_datainfo(data.info)
 gmodel = glm.fit.OLSModel(design, data)
 
 # Housekeeping and rescaling
-fl_contrast_names = ['Mean', 'linear', 'task', 'badsegments', 'blinks']
+fl_contrast_names = ['Mean', 'Linear Trend', 'Eyes Open>Closed', 'Bad Segments', 'VEOG', 'HEOG']
 gl_contrast_names = gmodel.contrast_names
 
-with h5py.File(os.path.join(outdir, 'lemon-group_glm-data.hdf5'), 'w') as F:
+with h5py.File(os.path.join(cfg['lemon_analysis_dir'], 'lemon-group_glm-data.hdf5'), 'w') as F:
      gmodel.to_hdf5(F.create_group('model'))
      design.to_hdf5(F.create_group('design'))
      #data.to_hdf5(F.create_group('data'))
 
-fout = os.path.join(outdir, 'lemon-group_glm-design.png')
+fout = os.path.join(cfg['lemon_analysis_dir'], 'lemon-group_glm-design.png')
 design.plot_summary(show=False, savepath=fout)
-fout = os.path.join(outdir, 'lemon-group_glm-efficiency.png')
+fout = os.path.join(cfg['lemon_analysis_dir'], 'lemon-group_glm-efficiency.png')
 design.plot_efficiency(show=False, savepath=fout)
 
 #%% ------------------------------------------------------
@@ -109,11 +110,12 @@ to_permute = [(0, 0, 'Overall Mean'),
               (0, 1, 'Group Mean of Linear Trend'),
               (0, 2, 'Group Mean of Task Effect'),
               (0, 3, 'Group Mean of Bad Segments'),
-              (0, 4, 'Group Mean of Blinks'),
-              (1, 0, 'Group Effect of Blinks on Mean'),
+              (0, 4, 'Group Mean of VEOG'),
+              (0, 5, 'Group Mean of HEOG'),
+              (1, 0, 'Group Effect of NumBlinks on Mean'),
               (2, 0, 'Group Effect of Sex on Mean')]
 
-run_perms = False
+run_perms = True
 for icon in range(len(to_permute)):
     if run_perms:
         gl_con = to_permute[icon][0]
@@ -122,19 +124,19 @@ for icon in range(len(to_permute)):
         fl_mean_data = deepcopy(data)
         fl_mean_data.data = data.data[:, fl_con, : ,:]
 
-        p = glm.permutations.MNEClusterPermutation(design, fl_mean_data, gl_con, 1000,
+        p = glm.permutations.MNEClusterPermutation(design, fl_mean_data, gl_con, 100,
                                                    nprocesses=3,
                                                    metric='tstats',
                                                    cluster_forming_threshold=cft,
                                                    tstat_args=tstat_args,
                                                    adjacency=adjacency)
 
-        with open(os.path.join(outdir, 'lemon-group_perms-con{0}.pkl'.format(icon)), "wb") as dill_file:
+        with open(os.path.join(cfg['lemon_analysis_dir'], 'lemon-group_perms-con{0}.pkl'.format(icon)), "wb") as dill_file:
             dill.dump(p, dill_file)
 
         P.append(p)
     else:
-        dill_file = os.path.join(outdir, 'lemon-group_perms-con{0}.pkl'.format(icon))
+        dill_file = os.path.join(cfg['lemon_analysis_dir'], 'lemon-group_perms-con{0}.pkl'.format(icon))
         P.append(dill.load(open(dill_file, 'rb')))
 
 #%% ----------------------------
@@ -185,59 +187,65 @@ def plot_design(ax, design_matrix, regressor_names):
     return cax
 
 I = np.argsort(data.data[:, 0, :, 23].sum(axis=1))
+I = np.arange(48)
 
 plt.figure(figsize=(16, 9))
+aspect = 16/9
+xf = qlt.prep_scaled_freq(0.5, freq_vect)
 
-subj_ax = plt.axes([0.08, 0.225, 0.2279411764705882, 0.77])
-des_ax = plt.axes([0.3985294117647059, 0.225, 0.2279411764705882, 0.77])
-cb_dm = plt.axes([0.64, 0.225, 0.01, 0.2])
+subj_ax = plt.axes([0.05, 0.225, 0.35, 0.35*aspect])
+des_ax = plt.axes([0.425, 0.225, 0.175, 0.51])
+cb_dm = plt.axes([0.62, 0.225, 0.01, 0.2])
 mean_ax = plt.axes([0.75, 0.6, 0.23, 0.25])
 cov_ax = plt.axes([0.75, 0.1, 0.23, 0.25])
 
-subj_ax.plot((0,25*24), (0, 1e-12*24), color=[0.8, 0.8, 0.8])
-for ii in range(24):
+subj_ax.plot((0, 35*len(I)), (0, 2e-4*len(I)), color=[0.8, 0.8, 0.8], lw=0.5)
+for ii in range(48):
     d = data.data[I[ii],0, :, 23]
-    subj_ax.plot(np.arange(96)+25*ii, d + 1e-12*ii)
+    subj_ax.plot(np.arange(len(freq_vect))+35*ii, d + 2e-4*ii)
 for tag in ['top','right']:
     subj_ax.spines[tag].set_visible(False)
-subj_ax.spines['bottom'].set_bounds(0, 96)
-subj_ax.spines['left'].set_bounds(0, 0.5e-11)
+subj_ax.spines['bottom'].set_bounds(0, len(freq_vect))
+subj_ax.spines['left'].set_bounds(0, 0.1e-2)
 subj_ax.set_xlim(0)
 subj_ax.set_ylim(0)
-subj_ax.set_xticks([0, 48, 96], [0, 24, 48])
-subj_ax.set_yticks([0, 0.5e-11])
-l = subj_ax.set_xlabel('Frequency (Hz)')
-l.set_position((0.1, 44.76166666666667))
-l = subj_ax.set_ylabel('Power')
-l.set_position((41.91666666666667, 0.05))
-subj_ax.text(48+25*16, 1e-12*12, r'Participants $\rightarrow$', rotation=55)
-qlt.subpanel_label(subj_ax, chr(65), yf=0.75)
+subj_ax.set_xticks([])
+subj_ax.set_yticks([])
+l = subj_ax.set_xlabel(r'Frequency (Hz) $\rightarrow$', loc='left')
+l = subj_ax.set_ylabel(r'Amplitude $\rightarrow$', loc='bottom')
+subj_ax.text(48+35*19, 2e-4*12, r'Participants $\rightarrow$', rotation=45)
+subj_ax.set_title('First level GLM Spectra')
+qlt.subpanel_label(subj_ax, chr(65), yf=1.05, xf=0.25)
 
 
-pcm = plot_design(des_ax, design.design_matrix[I[:24], :], design.regressor_names)
-des_ax.set_ylim(0, 37.5)
+pcm = plot_design(des_ax, design.design_matrix[I, :], design.regressor_names)
 des_ax.spines['left'].set_bounds(0, 24)
-des_ax.set_yticks(np.arange(5)*5)
+des_ax.set_yticks(np.arange(10)*5)
 des_ax.set_xticklabels(['Mean', 'Num Blinks', 'Sex', ''])
 des_ax.set_ylabel('Participants')
-qlt.subpanel_label(des_ax, chr(65+1), yf=0.75)
+des_ax.set_title('Group Design Matrix')
+qlt.subpanel_label(des_ax, chr(65+1), yf=1.05)
 plt.colorbar(pcm, cax=cb_dm)
 
 
-mean_ax.errorbar(freq_vect, gmodel.copes[0, 0, :, 23], yerr=np.sqrt(gmodel.varcopes[0, 0, :, 23]), errorevery=2)
+mean_ax.errorbar(xf[0], gmodel.copes[0, 0, :, 23], yerr=np.sqrt(gmodel.varcopes[0, 0, :, 23]), errorevery=1)
+mean_ax.set_xticks(xf[2], xf[1])
 mean_ax.set_title('Group Mean Spectrum')
-qlt.decorate_spectrum(mean_ax)
+qlt.decorate_spectrum(mean_ax, ylabel='Amplitude')
 qlt.subpanel_label(mean_ax, chr(65+2), yf=1.1)
+mean_ax.set_xlim(0)
+mean_ax.set_ylim(0)
 
-cov_ax.errorbar(freq_vect, gmodel.copes[1, 0, :, 23], yerr=np.sqrt(gmodel.varcopes[1, 0, :, 23]), errorevery=2)
-cov_ax.set_title('Group effects')
-cov_ax.errorbar(freq_vect, gmodel.copes[2, 4, :, 23], yerr=np.sqrt(gmodel.varcopes[1, 4, :, 23]), errorevery=2)
+cov_ax.errorbar(xf[0], gmodel.copes[1, 0, :, 23], yerr=np.sqrt(gmodel.varcopes[1, 0, :, 23]), errorevery=2)
+cov_ax.set_title('Group effects on Mean Spectrum')
+cov_ax.errorbar(xf[0], gmodel.copes[2, 4, :, 23], yerr=np.sqrt(gmodel.varcopes[1, 4, :, 23]), errorevery=2)
+cov_ax.set_xticks(xf[2], xf[1])
 cov_ax.legend(['Blinks', 'Sex'])
-qlt.decorate_spectrum(cov_ax)
+qlt.decorate_spectrum(cov_ax, ylabel='Amplitude')
 qlt.subpanel_label(cov_ax, chr(65+3), yf=1.1)
 
 
-fout = os.path.join(outdir, 'lemon-group_glm-overview.png')
+fout = os.path.join(cfg['lemon_analysis_dir'], 'lemon-group_glm-overview.png')
 plt.savefig(fout, transparent=True, dpi=300)
 
 #%% ----------------------------
@@ -272,7 +280,7 @@ for ii in range(2):
     qlt.plot_sensorspace_clusters(fl_mean_data, P[ii+5], raw, ax, base=0.5, title=to_permute[ii+5][2], ylabel='t-stat', xvect=freq_vect)
     qlt.subpanel_label(ax, chr(65+ii+6), yf=1.1)
 
-fout = os.path.join(outdir, 'lemon-group_glm-clusters.png')
+fout = os.path.join(cfg['lemon_analysis_dir'], 'lemon-group_glm-clusters.png')
 plt.savefig(fout, transparent=True, dpi=300)
 
 #%% ----------------------------
